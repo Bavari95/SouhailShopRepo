0ba6da5d6837c20377c9ca740d3f83bd2d7292ae
Ahmed <benhammoudaahmed95@gmail.com>
2021-02-01 20:56:46 +0100

    AddBrandsAndTypesMethods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,5 +34,19 @@ namespace API.Controllers
             return await _repo.GetProductByIdAsync(id);
         }
 
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<Product>>> GetProductBrands()
+        {
+            var productBrands = await _repo.GetProductBrandsAsync();
+            return Ok(productBrands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<Product>>> GetProductTypes()
+        {
+            var productTypes = await _repo.GetProductTypesAsync();
+            return Ok(productTypes);
+        }
+
     }
 }
