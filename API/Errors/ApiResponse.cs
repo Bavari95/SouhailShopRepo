f4d1fb3f93d4924d582c461b436b5333a132bf83
Ahmed <benhammoudaahmed95@gmail.com>
2021-02-21 18:47:09 +0100

    TestController + ErrorResponse

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request you have maid",
+                401 => "Authorized you are not",
+                404 => "Not found",
+                500 => "Server error",
+                _ => null
+            };
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
