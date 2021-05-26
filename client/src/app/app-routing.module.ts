a740801deb9584e1862949d044203bf5ab6f9159
Ahmed <benhammoudaahmed95@gmail.com>
2021-05-26 22:17:55 +0100

    Add error-test component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ShopComponent } from './shop/shop.component';
@@ -7,6 +8,7 @@ import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
   {path: 'shop', component: ShopComponent},
   {path: 'shop/:id', component: ProductDetailsComponent},
   {path: '**', redirectTo: '', pathMatch : 'full'}
