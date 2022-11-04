4d60c19feb38a9ad8d57e1484908481a08154ffb
DESKTOP-EIH1U9D\Bavari <benhammoudaahmed95@gmail.com>
2022-11-04 22:54:16 +0100

    test

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,7 @@ namespace API
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
+            
             _config = config;
             
             // Configuration = configuration;
